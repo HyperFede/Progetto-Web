420f0ab310fa39697a84b7940ac589933029eafc
Enryco-04 <enricostefanazzi18@gmail.com>
2025-06-04 01:26:45 +0200

    Basic reviewRoutes con doc, comprese immagini e filtri

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const paymentRoutes = require('./routes/paymentRoutes'); // Importa le route dei
 const authRoutes = require('./routes/authRoutes'); // Importa le route di autenticazione
 const cartRoutes = require('./routes/cartRoutes'); // Importa le route del carrello
 const subOrderRoutes = require('./routes/subOrderRoutes'); // Importa le route degli ordini secondaris
-
+const reviewRoutes = require('./routes/reviewRoutes'); // Importa le route delle recensioni
 const app = express();
 app.use(cors()); // Abilita CORS per tutte le richieste
 app.use(cookieParser()); // Usa il middleware per il parsing dei cookie
@@ -44,7 +44,7 @@ app.use('/api/payments', paymentRoutes); // Monta le route dei pagamenti
 app.use('/api/auth', authRoutes); // Monta le route di autenticazione
 app.use('/api/cart', cartRoutes); // Monta le route del carrello
 app.use('/api/suborders', subOrderRoutes); // Monta le route degli ordini secondari
-
+app.use('/api/reviews', reviewRoutes); // Monta le route delle recensioni
 
 // Endpoint di root per un semplice check
 app.get('/', (req, res) => {
