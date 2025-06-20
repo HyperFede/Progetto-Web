7eb244d8e1afbe1f1efd12cb9d131162eda716ff
HyperFede <federico.mauro04@gmail.com>
2025-06-20 16:10:17 +0200

    emailSender con F2A

--- frontend/js/emailSender.js
+++ frontend/js/emailSender.js
@@ -1,20 +1,42 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-export default function sendEmail(dest, sub, text){
+function sendEmail(dest, sub, text, attach = false){
     let transport = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'bazartcommerce@gmail.com',
-            pass: 'BazArtTiaStefFede06%'
+            type: 'OAuth2',
+            user: process.env.EMAIL,
+            clientId: process.env.EMAIL_CLIENT_ID,
+            clientSecret: process.env.EMAIL_SECRET,
+            refreshToken: process.env.EMAIL_REFRESH,
         }
     });
+
+    let mailOpt;
+
+    if(!attach){
+        mailOpt = {
+            from: 'bazartcommerce@gmail.com',
+            to: dest,
+            subject: sub,
+            html: text
+        };
+    }else{
+        mailOpt = {
+            from: 'bazartcommerce@gmail.com',
+            to: dest,
+            subject: sub,
+            html: text,
+            attachments: [
+                {
+                    filename: "Logo.svg",
+                    path: "../assets/Logo.svg",
+                    cid: "logo@example.com"
+                }
+            ]
+        };
+    }
     
-    let mailOpt = {
-        from: 'bazartcommerce@gmail.com',
-        to: dest,
-        subject: sub,
-        html: text
-    };
 
     transport.sendMail(mailOpt, (error, info) => {
         if(error){
