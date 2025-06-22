73277f1d5b461bdea9cbbb83792cc6f92dbb2786
HyperFede <federico.mauro04@gmail.com>
2025-06-23 01:53:11 +0200

    Final fix: authMiddleWare

--- backend/src/middleware/authMiddleWare.js
+++ backend/src/middleware/authMiddleWare.js
@@ -9,6 +9,10 @@ const jwtSecret = process.env.JWT_SECRET;
 
 // Controllo critico: se JWT_SECRET non è definito, l'applicazione non può funzionare in modo sicuro.
 // Stampa un errore fatale e termina il processo.
+if (!jwtSecret) {
+    console.error('FATAL ERROR: JWT_SECRET is not defined.');
+    jwtSecret = "Stef" // Termina l'applicazione se la chiave segreta non è configurata
+}
 
 /**
  * Funzione helper per recuperare un utente attivo basato su un token JWT.
